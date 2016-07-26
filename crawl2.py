6760c82a6899af63be8c88be053b47b792c56fd9
no limits but...sky <sky.wu.gss@gmail.com>
2016-07-26 11:54:53 +0800

    refactor and create new crawl2

--- crawl2.py
+++ crawl2.py
@@ -14,9 +14,58 @@ from os.path import isdir
 from urllib.request import urlopen
 import itertools
 import json
+import datetime
 
 import requests
 from lxml import html
+
+def str_date_to_int(date_str='20160712', esc_char='/'):
+    date_str = date_str.replace(esc_char, '')
+    year = int(date_str[:4])
+    month = int(date_str[4:6])
+    day = int(date_str[6:])
+    return year, month, day
+
+def str_to_date(date_str='20160712', esc_char=''):
+    year, month, day = str_date_to_int(date_str, esc_char)
+    return datetime.date(year, month, day)
+
+def to_taiwan_date_str(date_str='20160712', esc_char='/'):
+    year, month, day = str_date_to_int(date_str, esc_char)
+    taiwan_date = '{0}{esc_ch}{1:02d}{esc_ch}{2:02d}'.format(year - 1911, month, day, esc_ch=esc_char)
+    return taiwan_date
+
+def to_century_date_str(self, taiwan_date='105/07/12', esc_char='/'):
+    '''in:105/07/12, /  --> 2106/07/12
+       in:1050712  --> 20160712
+    '''
+    taiwan_date = taiwan_date.replace(esc_char, '')
+    year = int(taiwan_date[:3]) + 1911
+    month = int(taiwan_date[3:5])
+    day = int(taiwan_date[5:])
+    century_date = '{0}{esc_ch}{1:02d}{esc_ch}{2:02d}'.format(year, month, day, esc_ch=esc_char)
+    return century_date
+
+def date_range(start_date, end_date):
+    """ date_range(20160701, 20160712) """
+    # 若是字串則自動轉換為日期
+    start_date = str_to_date(start_date) if isinstance(start_date, str) else start_date
+    end_date = str_to_date(end_date)  if isinstance(end_date, str) else end_date
+
+    dlist = (start_date + datetime.timedelta(n) for n in range((end_date - start_date).days))
+    return dlist
+
+def download_data_by_url(url, fname):
+    # response = urlopen(url)
+    # data = response.read()
+    r = requests.get(url)
+    data = r.content
+
+    # need to use binary to get the data
+    # 原先的作法直接從request.response回來，需以binary的方式寫入
+    with open(fname, 'wb') as fd:
+        fd.write(data)
+        fd.close()
 
 class Crawler():
     def __init__(self, prefix="data", origin = "origin"):
@@ -57,121 +106,6 @@ class Crawler():
 
         return row #','.join(row)   #rdict #(zip(self.fields, row))   #row
 
-    def _record(self, stock_id, row):
-        ''' Save row to csv file '''
-        f = open('{}/{}.csv'.format(self.prefix, stock_id), 'ab')
-        cw = csv.writer(f, lineterminator='\n')
-        cw.writerow(row)
-        f.close()
-
-    def _get_tse_data(self, date_str):
-        payload = {
-            'download': '',
-            'qdate': date_str,
-            'selectType': 'ALL'
-        }
-        url = 'http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX.php'
-        # http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX.php?download=''&qdate=20160707&selectType=ALL
-
-        # Get html page and parse as tree
-        page = requests.post(url, data=payload)
-
-        if not page.ok:
-            logging.error("Can not get TSE data at {}".format(date_str))
-            return
-
-        # Parse page
-        tree = html.fromstring(page.text)
-
-        for tr in tree.xpath('//table[2]/tbody/tr'):
-            tds = tr.xpath('td/text()')
-
-            sign = tr.xpath('td/font/text()')
-            sign = '-' if len(sign) == 1 and sign[0] == u'－' else ''
-
-            row = self._clean_row([
-                date_str,  # 日期
-                tds[2],  # 成交股數
-                tds[4],  # 成交金額
-                tds[5],  # 開盤價
-                tds[6],  # 最高價
-                tds[7],  # 最低價
-                tds[8],  # 收盤價
-                sign + tds[9],  # 漲跌價差
-                tds[3],  # 成交筆數
-            ])
-
-            self._record(tds[0].strip(), row)
-
-    def _get_otc_data(self, date_str):
-        ttime = str(int(time.time() * 100))
-        url = 'http://www.tpex.org.tw/web/stock/aftertrading/daily_close_quotes/stk_quote_result.php?l=zh-tw&d={}&_={}'.format(
-            date_str, ttime)
-        page = requests.get(url)
-
-        if not page.ok:
-            logging.error("Can not get OTC data at {}".format(date_str))
-            return
-
-        result = page.json()
-
-        if result['reportDate'] != date_str:
-            logging.error("Get error date OTC data at {}".format(date_str))
-            return
-
-        for table in [result['mmData'], result['aaData']]:
-            for tr in table:
-                row = self._clean_row([
-                    date_str,
-                    tr[8],  # 成交股數
-                    tr[9],  # 成交金額
-                    tr[4],  # 開盤價
-                    tr[5],  # 最高價
-                    tr[6],  # 最低價
-                    tr[2],  # 收盤價
-                    tr[3],  # 漲跌價差
-                    tr[10]  # 成交筆數
-                ])
-                self._record(tr[0], row)
-
-    def get_data(self, year, month, day):
-        date_str = '{0}/{1:02d}/{2:02d}'.format(year - 1911, month, day)
-        print('Crawling {}'.format(date_str))
-        self._get_tse_data(date_str)
-        self._get_otc_data(date_str)
-
-    def _to_taiwan_date(self, date_str='20160712', esc_char='/'):
-        date_str = date_str.replace(esc_char, '')
-        year = int(date_str[:4])
-        month = int(date_str[4:6])
-        day = int(date_str[6:])
-        taiwan_date = '{0}{esc_ch}{1:02d}{esc_ch}{2:02d}'.format(year - 1911, month, day, esc_ch=esc_char)
-        return taiwan_date
-
-    def _to_century_date(self, taiwan_date='105/07/12', esc_char='/'):
-        '''in:105/07/12, /  --> 2106/07/12
-           in:1050712  --> 20160712
-        '''
-        taiwan_date = taiwan_date.replace(esc_char, '')
-        year = int(taiwan_date[:3])+1911
-        month = int(taiwan_date[3:5])
-        day = int(taiwan_date[5:])
-        century_date = '{0}{esc_ch}{1:02d}{esc_ch}{2:02d}'.format(year, month, day, esc_ch=esc_char)
-        return century_date
-
-    def _download_data_by_url(self, url, fname):
-        #response = urlopen(url)
-        #fname = '{}/{}.csv'.format(self.prefix, fname)
-        #data = response.read()
-        r = requests.get(url)
-        data = r.content
-
-        # need to use binary to get the data
-        # 原先的作法直接從request.response回來，需以binary的方式寫入
-        with open(fname, 'wb') as fd:
-            fd.write(data)
-            fd.close()
-
     def _get_url_of_tse_century(self, date_str):
         url = 'http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX3_print.php?'
         query_string = 'genpage=genpage/Report{quote_month}/A112{quote_date}ALL_1.php&type=csv'.format(
@@ -190,15 +124,17 @@ class Crawler():
     def _get_tse_data_raw(self, taiwan_date_str):
         url = self.url_tse.format(taiwan_date_str)
         fname = self._get_tse_file_name(self.origin, taiwan_date_str)
+        download_data_by_url(url, fname)
 
+    ''' get the csv directly'''
     def _get_tse_data_raw_simple(self, date_str='20170712'):
         url = self._get_url_of_tse_century(date_str)
         fname = '{}/{}_T.csv'.format(self.origin, date_str)
-        self._download_data_by_url(url, fname)
+        download_data_by_url(url, fname)
 
     def _transform_tse_data(self, taiwan_date_str):
         # Get html page and parse as tree
-        date_str = self._to_century_date(taiwan_date_str).replace('/', '')
+        date_str = to_century_date_str(taiwan_date_str).replace('/', '')
         infname = self._get_tse_file_name(self.origin, taiwan_date_str)
         infile = open(infname, 'rb')  # 'ab')
         data = infile.read()
@@ -234,54 +170,6 @@ class Crawler():
 
         f.close()
 
-    def _get_tse_data_all(self, date_str):
-        taiwan_date_str = str(int(date_str[:4]) - 1911) + date_str[5:]
-        payload = {
-            'download': '',
-            'qdate': taiwan_date_str,
-            'selectType': 'ALL'
-        }
-        url = 'http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX.php'
-        # http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX.php?download=''&qdate=20160707&selectType=ALL
-
-        # Get html page and parse as tree
-        page = requests.post(url, data=payload)
-
-        if not page.ok:
-            logging.error("Can not get TSE data at {}".format(date_str))
-            return
-
-        # Parse page
-        tree = html.fromstring(page.text)
-
-        '''for python3 2016/07/19'''
-        f = open('{}/{}_ALL.csv'.format(self.prefix, date_str), 'w') #'ab')
-        cw = csv.writer(f, lineterminator='\n')
-
-        for tr in tree.xpath('//table[2]/tbody/tr'):
-            tds = tr.xpath('td/text()')
-
-            sign = tr.xpath('td/font/text()')
-            sign = '-' if len(sign) == 1 and sign[0] == u'－' else ''
-
-            row = self._clean_row([
-                tds[0].strip(),  # symbol
-                date_str,  # 日期
-                tds[2],  # 成交股數
-                tds[4],  # 成交金額
-                tds[5],  # 開盤價
-                tds[6],  # 最高價
-                tds[7],  # 最低價
-                tds[8],  # 收盤價
-                sign + tds[9],  # 漲跌價差
-                tds[3],  # 成交筆數
-            ])
-
-            cw.writerow(row)
-            #f.write(row + "\n")
-
-        f.close()
-
     def _get_url_of_otc(self, taiwan_date_str):
         return (self.url_otc.format(taiwan_date_str))
 
@@ -294,12 +182,12 @@ class Crawler():
 
         fname = self._get_otc_file_name(self.origin, taiwan_date_str)
 
-        self._download_data_by_url(url, fname)
+        download_data_by_url(url, fname)
         return fname
 
     def _transform_otc_data(self, taiwan_date_str):
         # Get html page and parse as tree
-        date_str = self._to_century_date(taiwan_date_str).replace('/', '')
+        date_str = to_century_date_str(taiwan_date_str).replace('/', '')
         infname = self._get_otc_file_name(self.origin, taiwan_date_str)
         infile = open(infname, 'r')  # 'ab')
         data = infile.read()
@@ -334,8 +222,8 @@ class Crawler():
 
         f.close()
 
-    def _get_data_all_by_date(self, date_str='20160712'):
-        taiwan_date_str = self._to_taiwan_date(date_str, '/')
+    def get_data_all_by_date(self, date_str='20160712'):
+        taiwan_date_str = to_taiwan_date_str(date_str, '/')
         #print('Crawling {} by simple format'.format(date_str))
         # to-do: refactoring to general extract method
         #self._get_twse_data_raw_simple(date_str)
@@ -343,9 +231,6 @@ class Crawler():
         print('Crawling {} by as default'.format(taiwan_date_str))
         self._get_tse_data_raw(taiwan_date_str)
         self._get_otc_data_raw(taiwan_date_str)
-
-        # self._get_tse_data_all(date_str)
-        # self._get_otc_data_all(date_str)
 
         # to-to: transform the original file to expected format
         # _transform
@@ -357,16 +242,32 @@ class Crawler():
         # clean & load (delete the all and insert new one...
 
     def get_data_all(self, start_date='20160701', end_date='20160715'):
-        self._get_data_all_by_date(start_date)
+        max_error = 5
+        error_times = 0
+        # back to download
+        first_day = str_to_date(end_date)
+        last_day = str_to_date(start_date)
 
-    def _get_date_range(self):
+        while error_times < max_error and first_day >= last_day:
+            date_str = first_day.strftime('%Y%m%d')
+            print('Crawling {}'.format(date_str))
+            try:
+                self.get_data_all_by_date(date_str)
+                error_times = 0
+            except:
+                logging.error('Crawl raise error {}'.format(date_str))
+                error_times += 1
+                continue
+            finally:
+                first_day -= timedelta(1)
+
+    def _get_during(self):
         start_date = '20160701'
         end_date = '20160731'
         return start_date, end_date
 
     def _write_execution_log(self):
         pass
-
 
 '''
 args: crawl [start_date:yyyymmdd] [end_date:yyyymmdd]
@@ -382,10 +283,10 @@ def main():
                         format='%(asctime)s\t[%(levelname)s]\t%(message)s',
                         datefmt='%Y/%m/%d %H:%M:%S')
 
-    # Get arguments
+    # Get arguments of the during
     parser = argparse.ArgumentParser(description='Crawl data at assigned day')
     parser.add_argument('day', type=int, nargs='*',
-                        help='assigned day (format: YYYY MM DD), default is today')
+                        help='assigned day (format: YYYYMMDD), default is today')  # end_date
     parser.add_argument('-b', '--back', action='store_true',
                         help='crawl back from assigned day until 2004/2/11')
     parser.add_argument('-c', '--check', action='store_true',
@@ -396,42 +297,29 @@ def main():
     # Day only accept 0 or 3 arguments
     if len(args.day) == 0:
         first_day = datetime.today()
-    elif len(args.day) == 3:
-        first_day = datetime(args.day[0], args.day[1], args.day[2])
+    elif len(args.day) == 1:
+        first_day = str_to_date(str(args.day[0]))
     else:
         parser.error('Date should be assigned with (YYYY MM DD) or none')
         return
-
-    crawler = Crawler()
 
     # If back flag is on, crawl till 2004/2/11, else crawl one day
     if args.back or args.check:
         # otc first day is 2007/04/20
         # tse first day is 2004/02/11
-
-        last_day = datetime(2004, 2, 11) if args.back else first_day - timedelta(10)
-        max_error = 5
-        error_times = 0
-
-        while error_times < max_error and first_day >= last_day:
-            try:
-                crawler.get_data(first_day.year, first_day.month, first_day.day)
-                error_times = 0
-            except:
-                date_str = first_day.strftime('%Y/%m/%d')
-                logging.error('Crawl raise error {}'.format(date_str))
-                error_times += 1
-                continue
-            finally:
-                first_day -= timedelta(1)
+        last_day = str_to_date(str(args.day[1])) if args.back else first_day - timedelta(10)
     else:
-        crawler.get_data(first_day.year, first_day.month, first_day.day)
+        last_day = first_day
+
+    crawler = Crawler()
+    crawler.get_data_all(last_day, first_day)
 
 def test_download_all(start_date='20160712', end_date='20160712'):
     crawler = Crawler()
-    crawler.get_data_all(start_date, end_date)
+    crawler.get_data_all_by_date(start_date)
+    #crawler.get_data_all(start_date, end_date)
 
 if __name__ == '__main__':
     # main()
-    test_download_all('20160712', '20160712')
+    test_download_all('20160712', '20160713')
 
